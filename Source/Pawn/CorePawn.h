e9e7cb023701bc9a4e2a4ef6589df89ea159fc93
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-07-29 09:56:50 +0530

    Added base pawn class

---
+++ Source/Pawn/CorePawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CorePawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COREPLAYER_API ACorePawn : public APawn
+{
+	GENERATED_BODY()
+
+};
