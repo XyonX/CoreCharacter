937c72c71b5ce5bbf068dd72381f325763b83cbe
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-08-03 13:56:11 +0530

    Added uasset data ( Not Permanent )

---
+++ Source/CorePlayer/UAssetReference/CharacterUAssetData.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CharacterUAssetData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COREPLAYER_API UCharacterUAssetData : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	
+
+};
