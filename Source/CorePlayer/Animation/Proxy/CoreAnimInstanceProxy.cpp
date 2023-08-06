60a4dc496eab9443488ff69c2a5d63fa64d82501
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-08-06 12:26:45 +0530

    Modified proxy class

--- Source/CorePlayer/Animation/Proxy/CoreAnimInstanceProxy.cpp
+++ Source/CorePlayer/Animation/Proxy/CoreAnimInstanceProxy.cpp
@@ -7,7 +7,9 @@
 
 #include "CoreAnimInstanceProxy.h"
 #include "CorePlugin/Data/AnimationData.h"
-#include "Kismet/GameplayStatics.h"
+//#include "Kismet/GameplayStatics.h"
+#include "CorePlayer/Animation/CoreAnimInstance.h"
+#include "Kismet/KismetMathLibrary.h"
 
 
 FCoreAnimInstanceProxy::FCoreAnimInstanceProxy()
@@ -19,27 +21,67 @@ FCoreAnimInstanceProxy::FCoreAnimInstanceProxy(UCoreAnimInstance* InCoreAnimInst
 CoreAnimInstance(InCoreAnimInstance)
 
 {
-	// Initialize default values for AnimationData
-	//AnimationData.MovementSpeed = 0.0f;
-	//AnimationData.bIsJumping = false;
+	//DataLock = NewObject<FCriticalSection>(CoreAnimInstance->GetOwningActor());
 }
 
-
-bool FCoreAnimInstanceProxy::Evaluate(FPoseContext& Output)
+FCoreAnimInstanceProxy::~FCoreAnimInstanceProxy()
 {
-	// Lock the critical section for safe data access
-	//FScopeLock Lock(&CriticalSection);
 	
-	//Program Logic Base on the Anim Data
-
-
-
-	// Let the base FAnimInstanceProxy handle the rest of the animation evaluation
-	return FAnimInstanceProxy::Evaluate(Output);
 }
 
-void FCoreAnimInstanceProxy::UpdateAnimationData(const FAnimationData& NewAnimationData)
+void FCoreAnimInstanceProxy::Initialize(UAnimInstance* InAnimInstance)
 {
-	// Update the AnimationData with the provided data from the anim instance
-	AnimationData = NewAnimationData;
+	Super::Initialize(InAnimInstance);
+
+	// Cast the base animation instaNce class to the custom animation instace class
+	CoreAnimInstance = Cast<UCoreAnimInstance>(InAnimInstance);
+
+	if (CoreAnimInstance)
+	{
+		// Sync any data from the animation instaNce to the proxy
+		SyncFrom(CoreAnimInstance);
+	}
 }
+
+void FCoreAnimInstanceProxy::PostEvaluate(UAnimInstance* InAnimInstance)
+{
+	FAnimInstanceProxy::PostEvaluate(InAnimInstance);
+}
+
+void FCoreAnimInstanceProxy::SyncFrom(UCoreAnimInstance* Instance)
+{
+	// Sync any data from the animation instace to the proxy
+
+	// For example, copy the animation data from the animation instace to the proxy
+	RawAnimationData=Instance->RawAnimationData;
+}
+
+void FCoreAnimInstanceProxy::SyncTo(UCoreAnimInstance* Instance)
+{
+	Instance->CalculatedAnimationData=CalculatedAnimationData;
+}
+
+void FCoreAnimInstanceProxy::CalculateAnimationData()
+{
+	CalculatedAnimationData.Velocity = RawAnimationData.Velocity.Size();
+	
+	FRotator VelocityRotation =UKismetMathLibrary::MakeRotFromX(RawAnimationData.Velocity);
+	FRotator DeltaRotation = UKismetMathLibrary::NormalizedDeltaRotator(RawAnimationData.ControllerRotation,VelocityRotation);
+
+	
+	MovementRotation = FRotator(VelocityRotation.Pitch,ControlRotation.Yaw-DeltaRotation.Yaw,VelocityRotation.Roll);
+
+	CalculatedAnimationData.MovementRotation = MovementRotation;
+}
+
+/*
+void FCoreAnimInstanceProxy::CalculateCharacterData()
+{
+	FRotator VelocityRotation =UKismetMathLibrary::MakeRotFromX(RawAnimationDataRef.Velocity);
+	FRotator DeltaRotation = UKismetMathLibrary::NormalizedDeltaRotator(RawAnimationDataRef.ControllerRotation,VelocityRotation);
+
+	
+	MovementRotation = FRotator(VelocityRotation.Pitch,ControlRotation.Yaw-DeltaRotation.Yaw,VelocityRotation.Roll);
+
+	CalculatedAnimationDataRef.MovementRotation = MovementRotation;
+}*/
--- Source/CorePlayer/Animation/Proxy/CoreAnimInstanceProxy.h
+++ Source/CorePlayer/Animation/Proxy/CoreAnimInstanceProxy.h
@@ -2,11 +2,12 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstanceProxy.h"
+#include "CorePlugin/Data/AnimationData.h"
 #include "CoreAnimInstanceProxy.generated.h"
 
 class UCoreAnimInstance;
 
-struct FAnimationData;
+struct FCalculatedAnimationData;
 class UThirdPersonAnimInstance;
 
 // Create a custom AnimInstanceProxy struct
@@ -20,24 +21,51 @@ public:
 	FCoreAnimInstanceProxy();
 	
 	FCoreAnimInstanceProxy(UCoreAnimInstance* InCoreAnimInstance);
+	~FCoreAnimInstanceProxy();
 
 
-	/**Actual function to calculate the character data */
-	virtual bool Evaluate(FPoseContext& Output) override;
+	//Overrides
+	
+	// Override the Initialize function to copy any data from the AnimInstance that you need for animation calculations
+	virtual void Initialize(UAnimInstance* InAnimInstance) override;
 
-	//** Setter Function to set the Animation Data */
-	void UpdateAnimationData(const FAnimationData& NewAnimationData);
+	// Override the PostEvaluate function to copy any data back to the game thread that you need for animation post-processing
+	virtual void PostEvaluate(UAnimInstance* InAnimInstance) override;
+	
+	//Sync
+	
+	// Sync any data from the anim instance to the proxy
+	void SyncFrom(UCoreAnimInstance* Instance);
 
+	// Sync any data from the proxy to the anim instance
+	void SyncTo(UCoreAnimInstance* Instance);
+
+
+	//Calculation Function
+	void CalculateAnimationData ();
 	
 protected:
 	
 	/** Animation data that can be accessed from the rendering thread */
-	FAnimationData AnimationData;
+	FCalculatedAnimationData CalculatedAnimationData;
+	FRawAnimationData RawAnimationData;
+
+	//CODE
+	FRotator MovementRotation ;
+	FRotator ControlRotation;
+	FRotator CharacterRotation;
+	
 
 
 	/** Reference to the owning UMyAnimInstance */
 	UPROPERTY()
 	UCoreAnimInstance* CoreAnimInstance;
+	
+	//FThreadSafeBool bDataUpdated;
+
+	//FCriticalSection* DataLock;
+	// Use a scoped lock for synchronization
+	//FScopedLock DataLock;
 	
 	/** Critical section for synchronization */
 	//FCriticalSection CriticalSection;
@@ -45,3 +73,5 @@ protected:
 	/** Critical section for synchronization */
 	//mutable FCriticalSection CriticalSection;
 };
+
+
